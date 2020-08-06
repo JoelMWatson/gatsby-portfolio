c44fbb0b22d0b5daca39a2f91a294fc0f36fc7cb
Joel Watson <joel.watson92@gmail.com>
2020-08-06 02:32:28 -0700

    Made portfolio from gatsby starter

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,15 +1,18 @@
 module.exports = {
-  siteTitle: 'Hi! I\'m Francisco!',
-  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
-  keyWords: ['gatsbyjs', 'react', 'curriculum'],
-  authorName: 'Francisco Santos',
-  twitterUsername: '_franciscodf',
-  githubUsername: 'santosfrancisco',
+  siteTitle: 'Joel Watson',
+  siteDescription: `Joel Watson - Full-Stack Software Developer`,
+  keyWords: ['gatsbyjs', 'react'],
+  authorName: 'Joel Watson',
+  twitterUsername: 'joel3rbear',
+  githubUsername: 'joelmwatson',
   authorAvatar: '/images/avatar.jpeg',
-  authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
-    In 2012 I started working as a support technician and I approached the developers.
-    In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
-    I currently work as a frontend developer and mainly work with <strong>Javascript, NodeJS e React.</strong>`,
+  authorDescription: `Hi, I'm Joel, thanks for visiting my site. I'm a Full-Stack 
+  Developer working primarily in web and mobile application development. As a life-long 
+  learner, I'm always looking to expand and deepen my skill set as a developer. 
+  One thing about me is I am a true team player at heart and know that is where I 
+  thrive. I take an enormous amount of ownership in my team's outcomes and am always 
+  willing to put in the effort to ensure we attain our goals. I don’t quit till 
+  the job is done.`,
   skills: [
     {
       name: 'HTML',
@@ -40,66 +43,49 @@ module.exports = {
   jobs: [
     /* more jobs here */
     {
-      company: "Gympass",
-      begin: {
-        month: 'sep',
-        year: '2019'
-      },
-      duration: null,
-      occupation: "Frontend developer",
-      description: "I am part of the Corporate team, responsible for the development and maintenance of the employee management platform, giving more and more autonomy to partner companies."
-  
-    },  {
-      company: "Lendico",
-      begin: {
-        month: 'apr',
-        year: '2018'
-      },
-      duration: null,
-      occupation: "Frontend developer",
-      description: "I integrate the Frontend team responsible for developing and maintaining the online lending platform."
-  
-    }, {
-      company: "Anapro",
+      company: "TourVista",
       begin: {
         month: 'dec',
-        year: '2016'
+        year: '2017'
       },
-      duration: '1 yr e 5 mos',
-      occupation: "Fullstack developer",
-      description: "Development and maintenance, corrective and preventive, of web applications for the real estate market."
-    }, {
-      company: "Anapro",
+      duration: '6 months',
+      occupation: "Intern Developer",
+      description: "This was my first role in a technical position. I was mostly in charge of editing content on client websites through WordPress."
+
+    },
+    {
+      company: "TourVista",
       begin: {
-        month: 'set',
-        year: '2012'
+        month: 'jun',
+        year: '2017'
       },
-      duration: '4 yrs e 3 mos',
-      occupation: "Support Technician",
-      description: "Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool."
-  
+      duration: '2.5 years',
+      occupation: "Web Developer",
+      description: "After graduating I was hired on as an employee at TourVista. My responsibilities expanded to developing new tools for clients built in PHP and JS."
+
     },
   ],
   portifolio: [
     {
-      image: "/images/gatsby-starter-cv.png",
-      description: "Gatsby starter CV template",
-      url: "https://www.gatsbyjs.org/starters/santosfrancisco/gatsby-starter-cv/"
+      image: "/images/dndscribe.png",
+      description: "DnD Scribe Terminal App",
+      url: "https://www.npmjs.com/package/dnd-scribe"
     },
     {
-      image: "/images/awesome-grid.png",
-      description: "Responsive grid for ReactJS",
-      url: "https://github.com/santosfrancisco/react-awesome-styled-grid"
+      image: "/images/cocktailnotes.png",
+      description: "Cocktail Notes Web App",
+      url: "https://watson-cocktail-notes.herokuapp.com"
     },
-    /* more portifolio items here */
   ],
+
   social: {
-    twitter: "https://twitter.com/_franciscodf",
-    linkedin: "https://www.linkedin.com/in/santos-francisco",
-    github: "https://github.com/santosfrancisco",
-    email: "yoshi.df@gmail.com"
+    twitter: "https://twitter.com/joel3rbear",
+    linkedin: "https://www.linkedin.com/in/joel3rbear",
+    github: "https://github.com/joelmwatson",
+    email: "joel.watson92@gmail.com"
   },
-  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+
+  siteUrl: 'https://joelmwatson.github.io/',
   pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
   siteCover: '/images/cover.jpeg',
   googleAnalyticsId: 'UA-000000000-1',
